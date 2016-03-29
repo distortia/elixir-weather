76629a5f74ebb3f7cd6daa9a8d104cdbc8bddd72
shibuyamizou <nickstalter@gmail.com>
2016-03-28 21:46:48 -0400

    piped some shit

--- lib/weather.ex
+++ lib/weather.ex
@@ -1,18 +1,21 @@
 defmodule Weather do
+	# https://maps.googleapis.com/maps/api/geocode/json?address=1600+5233+jameson+drive+columbus+oh&key=AIzaSyDiglohQeePIsYHgHSLNqWeFhLg_xqxnV0
+	# https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE
 	def main(address) do
-		# https://maps.googleapis.com/maps/api/geocode/json?address=1600+5233+jameson+drive+columbus+oh&key=AIzaSyDiglohQeePIsYHgHSLNqWeFhLg_xqxnV0
-		# https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE
-		address = parse_address(to_string address)
-		coordinates = location(address)
-		weather(coordinates)
+		address 
+		|> to_string 
+		|> parse_address 
+		|> location 
+		|> weather
 	end
 
+	# Replaces spaces with +'s for google maps api
 	def parse_address(address) do
 		String.replace(address, " ", "+")
-	end
+	end	
 
+	# Gets the Weather for the location provided
 	def weather(location) do
-		IO.puts "location: #{location}"
 		{status, list} = JSON.decode request_weather(location).body
 		if status == :ok do
 			currently(list)
@@ -20,6 +23,7 @@ defmodule Weather do
 			IO.puts "Error: #{status} status returned"
 		end
 	end
+
 	# RETURNS lat/long coordinates from address
 	def location(address) do
 		{status, list} = JSON.decode request_coordinates(address).body
@@ -33,15 +37,18 @@ defmodule Weather do
 		end
 	end
 
+	# Puts the truncated current temperature for the location.
 	def currently(list) do
 		list = list["currently"]
 		IO.puts "It is currently #{trunc list["apparentTemperature"]} degrees"
 	end
 
+	# Hits the forecast.io api
 	def request_weather(location) do
 		HTTPotion.get "#{forcast_io_full_url(location)}"
 	end
 
+	# Hits the Google Maps API 
 	def request_coordinates(address) do
 		HTTPotion.get google_full_url(address)
 	end
@@ -54,17 +61,13 @@ defmodule Weather do
 		"#{forcast_io_base_url}/#{forcast_io_api_key}/#{location}"
 	end
 
+	def forcast_io_base_url do
+		"https://api.forecast.io/forecast"
+	end
+	
 	defp forcast_io_api_key do
 		"80f71455dfe8a1e882ba6adf75b6ccf1"
 	end
-
-	defp forcast_io_base_url do
-		"https://api.forecast.io/forecast"
-	end
-
-	# defp forcast_io_location do
-	# 	"39.926619,-82.85235899999999"
-	# end
 
 	defp google_api_key do
 		"AIzaSyDiglohQeePIsYHgHSLNqWeFhLg_xqxnV0"
